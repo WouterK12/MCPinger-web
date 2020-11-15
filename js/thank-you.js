0a070dbfbf421b099e30f876368f15c4bab52e37
WouterK12 <wouterkoopman1@gmail.com>
2020-11-15 22:59:41 +0100

    Updated tracker description, facts

--- js/thank-you.js
+++ js/thank-you.js
@@ -2,6 +2,7 @@ window.addEventListener("load", async () => {
   // variables
   const webAge = 8;
 
+  // jquery because of cors...
   await $.getJSON("https://api.mcsrvstat.us/2/play.wouterk12.com", (res) => {
     if (res.online) {
       status = "online!\nThat's awesome!";
@@ -16,15 +17,15 @@ window.addEventListener("load", async () => {
   // facts
   const facts = {
     pizza:
-      "My favourite food got to be pizza!\nIt probably stays nice because I don't eat it too often.\nThat could be true: a friend of mine ate pizza too frequently. Now, he doesn't like it anymore.\nI will never let that happen!",
+      "My favorite food has to be pizza!\nIt probably stays nice because I don't eat it too often.\nThat could be true: a friend of mine ate pizza too often. Now, he doesn't like it anymore.\nI will never let that happen!",
     owl:
-      "I like owls.\nIt's not that I have a specific favourite animal, but an owl is just facinating.\nBecause their head is shaped like a dish, they can hear the tiniest mouse under a thick layer of snow from far away!",
-    code: `I really like programming!\nMaybe you had discovered that too, but something you didn't know is that I created my first website when I was ${webAge}.\nThat is about ${
+      "I like owls.\nIt's not that I have a specific favourite animal, but an owl is just facinating.\nBecause they have saucer-shaped faces, they can hear the tiniest mouse under a thick layer of snow from far away!",
+    code: `I really like programming!\nMaybe you had discovered that too, but something you didn't know is that I created my first website when I was ${webAge} years old.\nThat is about ${
       new Date().getFullYear() - 2002 - webAge // 👀
-    } years ago!\nAnd of course, because I like programming: the number in the previous sentence is accurate and will change by itself. 😁`,
+    } years ago!\nAnd of course, because I like programming, the number in the previous sentence is accurate and will change by itself. 😁`,
     printer:
-      "In high school, I chose 'Computer Science' as an extra class.\n(In Dutch, that's 'Informatica')\nThere, together with a friend of mine, we created a 2D printer out of old CD drives.\nIt could print the name of the teacher with a marker, only one time! That was because there were lot's of loose contacts...\nBut, mr Delft really liked it, so we got a high score!",
-    minecraft: `Minecraft is probably the game I played the most.\nIt's sad that there is no overal playtime in Minecraft, but it got to be a lot.\nI created my own server in 2017, which currently is ${status}`,
+      "In high school, I chose 'Computer Science' as an extra class.\n(In Dutch, that's 'Informatica')\nThere, together with a friend of mine, we created a 2D printer out of old CD drives.\nIt could print the name of the teacher with a marker, only one time! That was because there were lots of loose contacts...\nBut, mr Delft really liked it, so we earned a high grade!",
+    minecraft: `Minecraft is probably the game I played the most.\nIt's sad that there is no overall playtime in Minecraft, but it got to be a lot.\nI created my own server in 2017, which currently is ${status}`,
     game:
       "At the moment of writing, I am really interested in game development.\nI love playing games with a great storyline and environment.\nMaybe games like 'The Walking Dead' and 'Shadow of the Tomb Raider' are some you've heard of. They're definitely worth trying!",
   };
