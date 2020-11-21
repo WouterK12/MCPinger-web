47b3492502011a61d9f0571a9769bd90f05a28f0
WouterK12 <wouterkoopman1@gmail.com>
2020-11-21 18:03:33 +0100

    Added search bar, verification mention

--- js/main.js
+++ js/main.js
@@ -39,4 +39,54 @@ window.addEventListener("load", () => {
       command.classList.remove("expanded");
     });
   }
+
+  // search bar
+  const commandsContainer = document.querySelector("article section.commands");
+  const searchInput = document.getElementById("searchInput");
+  let typing;
+
+  searchInput.addEventListener("keyup", () => {
+    // user is typing
+    clearTimeout(typing);
+
+    // hide all commands
+    commandsContainer.classList.add("hidden");
+
+    // after time, show results
+    typing = setTimeout(() => {
+      // search using input
+      const filter = searchInput.value.toLowerCase();
+      GetInput(filter);
+
+      // show commands
+      commandsContainer.classList.remove("hidden");
+    }, 500);
+  });
+
+  function GetInput(filter) {
+    let results = [];
+
+    // for every command
+    for (let i = 0; i < commands.length; i++) {
+      // get the keyword(s)
+      const keyword = commands[i].querySelector(".com");
+      txtValue = keyword.textContent || keyword.innerText;
+      // if filter is in keyword, show matches
+      if (txtValue.toLowerCase().includes(filter)) {
+        commands[i].style.display = "";
+        results.push(commands[i]);
+      } else {
+        commands[i].style.display = "none";
+      }
+    }
+
+    // if there is only one result
+    if (results.length == 1) {
+      results[0].classList.add("expanded");
+    }
+    // if there is more then one result
+    if (results.length > 1) {
+      CloseAll();
+    }
+  }
 });
